36fe82fd8da6d9948037af0ebd091ed1db688b92
franc <francoperino@hotmail.com.ar>
2017-12-17 21:44:26 -0300

    probalo ahora

--- disenio/src/daos/daoUsuario.java
+++ disenio/src/daos/daoUsuario.java
@@ -10,28 +10,28 @@ import Entidades.Usuario;
 public class daoUsuario {
     public static Usuario getUsuario(String nickusuario) throws Exception {
         try {
-            return ((ArrayList<Usuario>)((Object)Conexion.consultar(("SELECT * FROM Usuario WHERE Nickusuario = '" + nickusuario + "';"), Usuario.class))).get(0);
+            return ((ArrayList<Usuario>)((Object)Conexion.consultar(("SELECT * FROM Usuario WHERE Nickusuario = '" + nickusuario + "'COLLATE utf8_bin;"), Usuario.class))).get(0);
         }
         catch(Exception ex) {
             throw ex;
         }
     }
     public static ArrayList<Bedel> obtenerBedel(String nick) throws Exception {
-         String consulta = "select * from bedel b where b.nickusuario = '"+nick+"' ;";
+         String consulta = "select * from bedel b where b.nickusuario = '"+nick+"' COLLATE utf8_bin;";
           ArrayList<Bedel> res = (ArrayList<Bedel>)((Object)Conexion.consultar(consulta, Bedel.class));
           return res;
     }
     public static ArrayList<Admin> obtenerAdmin(String nick)throws Exception{
-        String consulta = "select * from Admin a where a.nickusuario = '"+nick+"' ;";
+        String consulta = "select * from Admin a where a.nickusuario = '"+nick+"' COLLATE utf8_bin;";
           ArrayList<Admin> res = (ArrayList<Admin>)((Object)Conexion.consultar(consulta, Admin.class));
           return res;
     }
     public static ArrayList<ConsultaGenerica> clave(String nick) throws Exception {
        
-        String consulta1 = "select MAX(c.idclave) AS maxid from clave c where c.nickusuario = '"+nick+"';";
+        String consulta1 = "select MAX(c.idclave) AS maxid from clave c where c.nickusuario = '"+nick+"'COLLATE utf8_bin;";
         ArrayList<ConsultaGenerica> res = (ArrayList<ConsultaGenerica>)((Object)Conexion.consultar(consulta1, ConsultaGenerica.class));
 		 Integer id = Integer.parseInt(res.get(0).getValor("maxid"));
-		String consulta = "select  c.claveactual from clave c where  c.idclave='"+Integer.parseInt(res.get(0).getValor("maxid"))+"';";
+		String consulta = "select  c.claveactual from clave c where  c.idclave='"+Integer.parseInt(res.get(0).getValor("maxid"))+"'COLLATE utf8_bin;";
         ArrayList<ConsultaGenerica> res1 = (ArrayList<ConsultaGenerica>)((Object)Conexion.consultar(consulta, ConsultaGenerica.class));
          
           return res1;
